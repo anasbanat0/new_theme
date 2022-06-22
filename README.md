9e00e086132a45d08f6b531121fa2dcb40d074a1
anas banat <53115475+anasali0@users.noreply.github.com>
2022-06-22 16:57:39 +0300

    This is my second theme

---
+++ README.md
@@ -0,0 +1,1 @@
+# WprdPress Theme - Aquila
---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Header template
+* @package Aquila
+*/
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>WordPress Theme</title>
+  </head>
+  <body>
+    <header>
+      header
+    </header>
